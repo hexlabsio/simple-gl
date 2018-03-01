21a88d772daa14755ebd88f5afffbdc7cc17ea38
bobjamin <bobjamin@live.co.uk>
2018-03-01 17:27:03 +0000

    fixed optimization of dynamic draw elements by toggling dynamic draw and by rendering at the object level instead of the scene level

--- src/gl-object.ts
+++ src/gl-object.ts
@@ -1,7 +1,14 @@
 import Vertex from './vertex'
 
+export enum GLOptimizedType{
+    TRIANGLES = WebGLRenderingContext.TRIANGLES,
+    TRIANGLE_FAN = WebGLRenderingContext.TRIANGLE_FAN,
+    LINES =  WebGLRenderingContext.LINES
+}
+
 export default abstract class GLObject{
     abstract verticies(): Array<Vertex>
+    optimizedType(): GLOptimizedType { return GLOptimizedType.TRIANGLES }
     setColor(color: number[]){
         this.verticies().forEach((vertex: Vertex) => vertex.color = color)
     }
