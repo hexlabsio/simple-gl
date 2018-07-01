5398a881fb431f592bc9ed4c73fe6ef4897e84bd
Chris Barbour <xbobjamin@gmail.com>
2018-07-01 21:47:40 +0100

    moved node into maven

--- src/test/kotlin/simpleGl/VectorTest.kt
+++ src/test/kotlin/simpleGl/VectorTest.kt
@@ -1,10 +1,10 @@
 package simpleGl
 
 import kotlin.test.Test
-import kotlin.test.assertEquals
+import kotlin.test.expect
+
+import simpleGl.Vector2
 
 class VectorTest{
-    @Test fun test(){
-        assertEquals(3, 2)
-    }
+    @Test fun translate2D() = expect(Vector2(10f, 25f)){ Vector2(0f, 0f) + Vector2(10f, 25f)}
 }
