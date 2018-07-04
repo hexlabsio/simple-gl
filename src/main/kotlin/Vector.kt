62e3ac900087a3edd1c9cc1fd09734d8706c6e56
Chris Barbour <xbobjamin@gmail.com>
2018-07-04 21:49:20 +0100

    adding basic circleci config

--- src/main/kotlin/Vector.kt
+++ src/main/kotlin/Vector.kt
@@ -3,26 +3,27 @@ import kotlin.math.sqrt
 
 typealias Color = Vector4
 
-data class Vector2(override val x: Float, override val y: Float): Vector(listOf(x, y))
-data class Vector3(override val x: Float, override val y: Float, override val z: Float): Vector(listOf(x, y, z))
-data class Vector4(override val r: Float, override val g: Float, override val b: Float, override val a: Float = 0f): Vector(listOf(r, g, b, a))
+data class Vector2(val x: Float, val y: Float): Vector(listOf(x, y))
+data class Vector3(val x: Float, val y: Float, val z: Float): Vector(listOf(x, y, z))
+data class Vector4(val r: Float, val g: Float, val b: Float, val a: Float = 0f): Vector(listOf(r, g, b, a))
 
 open class Vector(val components: List<Float>): ArrayList<Float>(components){
-    val length: Float by lazy { sqrt(this dot this) }
-    val direction: Vector by lazy {
-        if(length == 0f) Vector(components.map{ 0f })
-        else Vector(components.map { it / length })
+    private var _length: Float? = null
+    private var _direction: Vector? = null
+    fun length(): Float{
+        if(_length == null) _length = sqrt(this dot this)
+        return _length!!
     }
-    open val x: Float by lazy { components[0] }
-    open val y: Float by lazy { components[1] }
-    open val z: Float by lazy { components[2] }
-    open val u: Float by lazy { components[0] }
-    open val v: Float by lazy { components[1] }
-    open val w: Float by lazy { components[2] }
-    open val r: Float by lazy { components[0] }
-    open val g: Float by lazy { components[1] }
-    open val b: Float by lazy { components[2] }
-    open val a: Float by lazy { components[3] }
+    fun direction(): Vector {
+        if (_direction == null) _direction = with(length()) {
+            when {
+                this == 0f -> Vector(components.map { 0f })
+                else -> Vector(components.map { it / this })
+            }
+        }
+        return _direction!!
+    }
+
 }
 
 operator fun Vector.plus(v: Vector) = operateOn(v){ a, b -> a + b }
@@ -32,27 +33,27 @@ operator fun Vector.times(scale: Float) = Vector(components.map { it * scale })
 operator fun Float.times(v: Vector) = v * this
 infix fun Vector.dot(v: Vector) = this.times(v).components.sum()
 
-fun Vector.to2D() = Vector2(x, y)
-fun Vector.to3D() = Vector3(x, y, z)
-fun Vector.to4D() = Vector4(r, g, b, a)
+fun Vector.to2D() = Vector2(this[0], this[1])
+fun Vector.to3D() = Vector3(this[0], this[1], this[2])
+fun Vector.to4D() = Vector4(this[0], this[1], this[2], this[3])
 
 operator fun Vector2.plus(v: Vector2) = (this as Vector).plus(v).to2D()
 operator fun Vector2.minus(v: Vector2) = (this as Vector).minus(v).to2D()
 operator fun Vector2.times(v: Vector2) = (this as Vector).times(v).to2D()
 operator fun Vector2.times(scale: Float) = (this as Vector).times(scale).to2D()
-fun Vector2.direction() = this.direction.to2D()
+fun Vector2.direction() = this.direction().to2D()
 
 operator fun Vector3.plus(v: Vector3) = (this as Vector).plus(v).to3D()
 operator fun Vector3.minus(v: Vector3) = (this as Vector).minus(v).to3D()
 operator fun Vector3.times(v: Vector3) = (this as Vector).times(v).to3D()
 operator fun Vector3.times(scale: Float) = (this as Vector).times(scale).to3D()
-fun Vector3.direction() = this.direction.to3D()
+fun Vector3.direction() = this.direction().to3D()
 
 operator fun Vector4.plus(v: Vector4) = (this as Vector).plus(v).to4D()
 operator fun Vector4.minus(v: Vector4) = (this as Vector).minus(v).to4D()
 operator fun Vector4.times(v: Vector4) = (this as Vector).times(v).to4D()
 operator fun Vector4.times(scale: Float) = (this as Vector).times(scale).to4D()
-fun Vector4.direction() = this.direction.to4D()
+fun Vector4.direction() = this.direction().to4D()
 
 
 private fun Vector.operateOn(b: Vector, operation: (Float, Float) -> Float) = Vector((components to b.components).sequence().map{ it.safe(operation) })
